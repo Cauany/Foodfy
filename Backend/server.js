def54b9db40a4095993e3f33bcc9f1849845b826
Cauany Souza <souza.cauany99@gmail.com>
2020-04-07 16:42:37 -0300

    creating modal

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,42 @@
+//All require
 const express = require('express')
+const nunjucks = require('nunjucks')
 
+//Other constants
+
+//Start Server
 const server = express()
 
-server.get('/', function(req, res){
-    return res.send('All okay!')
+
+//Looking file public
+server.use(express.static('public'))
+
+//View Motor
+server.set("view engine", "njk")
+
+//Nunjucks configure
+nunjucks.configure("views", {
+    express: server,
+    
+    //For clean the cache
+    noCache: true
+
+    //Allowed to print the html
+    //autoescape: false 
 })
 
+//Routes configure
+server.get('/', function(req, res){
+    return res.render('index')
+})
 
+server.get('/about', function(req, res){
+    return res.render('about')
+})
 
-
-
-
+server.get('/recipe', function(req, res){
+    return res.render('recipe')
+})
 
 server.listen(5000, function(){
     console.log("It's running")
