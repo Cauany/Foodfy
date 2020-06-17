fd09425a325ce4f2e79c65740ad176e5be5582ea
Cauany Souza <souza.cauany99@gmail.com>
2020-06-17 11:52:41 -0300

    refatorando foodfy

--- Backend/public/scripts.js
+++ Backend/public/scripts.js
@@ -1,19 +1,2 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card-recipe')
-const recipes = document.querySelector('content-recipe-detail')
 
-for (let card of cards){
-    card.addEventListener("click", function() {
-    const linkId = card.getAttribute('id')
-
-    modalOverlay.classList.add('active')
-    modalOverlay.querySelector("img").src = `/assets/${linkId}.png`
-    modalOverlay.querySelector(".but").href = `/recipeDetail?id=${linkId}`
-})
-}
-    document.querySelector('.close-modal').addEventListener("click", function() {
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector("img").src = ""
-    modalOverlay.querySelector(".but").href = ""
-})
 
